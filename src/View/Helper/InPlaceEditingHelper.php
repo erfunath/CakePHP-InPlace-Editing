47740357dc045359eb8e40ef6d015f7375cdd0e2
erfunath <4219124+erfunath@users.noreply.github.com>
2019-09-08 10:31:02 -0800

    Customizations
    Adds functionality that original did not have.

--- src/View/Helper/InPlaceEditingHelper.php
+++ src/View/Helper/InPlaceEditingHelper.php
@@ -3,6 +3,8 @@
 namespace InPlaceEditing\View\Helper;
 
 use Cake\View\Helper;
+use Cake\Log\Log;
+use Cake\Error\Debugger;
 
 /*
  * ----------------------------------------------------------------------------
@@ -23,7 +25,7 @@ use Cake\View\Helper;
 class InPlaceEditingHelper extends Helper
 {
 
-    public $helpers = ['Html'];
+    public $helpers = ['Html', 'Url'];
 
     /*
      * Returns a script which contains a html element (type defined in a parameter) with the field contents.
@@ -33,40 +35,68 @@ class InPlaceEditingHelper extends Helper
     {
         $value         = $this::extractSetting($settings, 'value', '');
         $actionName    = $this::extractSetting($settings, 'actionName', 'inPlaceEditing');
-        $type          = $this::extractSetting($settings, 'type', 'textarea');
-        $cancelText    = $this::extractSetting($settings, 'cancelText', 'Cancel');
-        $submitText    = $this::extractSetting($settings, 'submitText', 'Save');
+        $type          = $this::extractSetting($settings, 'type', 'text');
+        $cancelText    = $this::extractSetting($settings, 'cancelText', '');
+        $submitText    = $this::extractSetting($settings, 'submitText', '');
         $toolTip       = $this::extractSetting($settings, 'toolTip', 'Click to edit.');
         $containerType = $this::extractSetting($settings, 'containerType', 'div');
         $csrfToken     = $this::extractSetting($settings, 'csrfToken', '');
         $rows          = $this::extractSetting($settings, 'rows', '1');
+        $data          = $this::extractSetting($settings, 'data', '');
+        $updateMaxPoints = $this::extractSetting($settings, 'updateMaxPoints', 'false');
+        $updateID      = $this::extractSetting($settings, 'updateID', '');
         
         //remove quotation marks
         $csrfToken = trim($csrfToken, '"');
+        $complete = '';
+        $base_route = '';
         
+        $route = $this->Url->build($actionName, true);
+
+        if ($updateMaxPoints == 'true' && $updateID != '') {
+            $base_route = $this->Url->build(['controller' => 'Rubrics', 'action' => 'getMaxPoints'], true);
+            $complete = "complete: function() {
+                $.ajax({
+                    type: 'GET',
+                    url: '$base_route/$updateID',
+                    dataType: 'html',
+                    beforeSend: function(xhr) {
+                        xhr.setRequestHeader('X-CSRF-Token',
+                            '<?php echo $csrfToken ?>');
+                    },
+                    success: function(response) {
+                        $('#rubric-$updateID-max-points').html(response);
+                        
+                    },
+                });
+            },";
+        }
+
         $elementID = 'inplace_'.$modelName.'_'.$fieldName.'_'.$id;
-        $input     = '<'.$containerType.' id="'.$elementID.'" class="in_place_editing">'.$value.'</'.$containerType.'>';
+        $input     = '<'.$containerType.' id="'.$elementID.'" class="in_place_editing">'.$value.'</'.$containerType.'>';        
         $script    = "$(function(){
-                        $('#$elementID').editable(
-                            '..$actionName/$id', 
-                            {
+                        $('#$elementID').editable('$route/$id', {
                                 name      : '$fieldName',
                                 type      : '$type',
                                 cancel    : '$cancelText',
                                 submit    : '$submitText',
                                 tooltip   : '$toolTip',
                                 rows      : '$rows',
+                                data      : '$data',
+                                cssclass  : 'ui-widget',
+                                submitcssclass: 'ui-button ui-widget ui-corner-all',
+                                cancelcssclass: 'ui-button ui-widget ui-corner-all',
+                                indicator : '<div class=\'ajaxsaving\'></div>',
                                 ajaxoptions : {
                                     beforeSend: function(xhr){
                                         xhr.setRequestHeader('X-CSRF-Token', '$csrfToken');
                                     },
+                                $complete
                                 }
                             }
                         );
                     });";
-
-        $this->Html->scriptBlock($script, ['block' => true]);
-
+        $this->Html->scriptBlock($script, ['block' => 'scriptBottom']);
         return $input;
     }
 
